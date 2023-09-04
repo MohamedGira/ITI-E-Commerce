ebc6ce00ca203c9dad1484939e149a4ac5a0f73b
MohamedGira <mohamedgira0901@gmail.com>
2023-09-04 03:17:15 +0300

    category recursive search

--- resources/views/customer/category.blade.php
+++ resources/views/customer/category.blade.php
@@ -4,7 +4,7 @@
         <div class="p-10 bg-gray-100">
             @if (count($item->subCategories))
                 <h2 class="text-2xl text-gray-500 mb-1">Sub Categories</h2>
-                <ul class="flex flex-start gap-12 menu ">
+                <ul class="flex flex-start gap-12 menu mb-12 ">
                     @foreach ($item->subCategories as $category)
                         <li>
                             <x-myComponents.category-card :category="$category" />
@@ -12,16 +12,40 @@
                     @endforeach
                 </ul>
             @endif
-            <div class="mt-12">
+            <div class="">
                 <h2 class="text-2xl text-gray-500 mb-1">Products</h2>
                 <ul class="flex flex-start gap-12 menu ">
-                    @foreach ([$item, ...$item->subCategories] as $category)
+                    @php
+                        if (!function_exists('getSubCategories')) {
+                            function getSubCategories($item)
+                            {
+                                $arr = [];
+                                if (count($item->subCategories) > 0) {
+                                    foreach ($item->subCategories as $subCategory) {
+                                        $arr = [...$arr, $subCategory, ...getSubCategories($subCategory)];
+                                    }
+                                }
+                                return $arr;
+                            }
+                            $subCategories = [$item, ...getSubCategories($item)];
+                        } else {
+                            $subCategories = [$item, ...getSubCategories($item)];
+                        }
+                        $v = 0;
+                    @endphp
+                    @foreach ($subCategories as $category)
                         @foreach ($category->productsCategories as $productCategory)
                             <li>
                                 <x-myComponents.product-card :product="$productCategory->product" />
                             </li>
+                            @php
+                                $v++;
+                            @endphp
                         @endforeach
                     @endforeach
+                    @if ($v == 0)
+                        <h1 class="text-5xl self-center w-screen text-center text-gray-500">No Products Found</h1>
+                    @endif
                 </ul>
             </div>
         </div>
